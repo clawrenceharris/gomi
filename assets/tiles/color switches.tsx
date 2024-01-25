8cbfd83d7053b4cfc2482abd2c7ca76122211381
Caleb Harris <caleb@Calebs-Air.lan>
2024-01-24 21:34:36 -0500

    refactor/updated level tileset references

--- assets/tiles/color switches.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="color switchers" tilewidth="24" tileheight="24" tilecount="4" columns="4">
- <image source="../images/color-switchers.png" width="96" height="24"/>
-</tileset>
--- assets/tiles/water bottle.tsx
+++ assets/tiles/water bottle.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="water bottle" tilewidth="18" tileheight="26" tilecount="3" columns="3">
- <image source="../images/water-bottle.png" width="54" height="26"/>
+ <image source="../images/Enemies/Water Bottle/Idle.png" width="54" height="26"/>
 </tileset>
