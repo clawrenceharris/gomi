51497cc7f67f0342deb81894f68429864a712f18
Caleb Harris <caleb@Calebs-Air.lan>
2024-02-17 18:12:52 -0500

    updated level order

--- lib/level_selection/levels.dart
+++ lib/level_selection/levels.dart
@@ -1,5 +1,5 @@
 const gameLevels = <GameLevel>[
-  (number: 1, pathname: 'level-2.tmx', winScore: 0, hasInfoTiles: false),
+  (number: 1, pathname: 'level-1.tmx', winScore: 0, hasInfoTiles: true),
   (number: 2, pathname: 'level-2.tmx', winScore: 0, hasInfoTiles: false),
   (number: 3, pathname: 'level-3.tmx', winScore: 0, hasInfoTiles: false),
   (number: 4, pathname: 'level-4.tmx', winScore: 0, hasInfoTiles: false),
