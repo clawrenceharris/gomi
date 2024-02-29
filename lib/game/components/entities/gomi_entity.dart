5df9997892b96c3850633da36ea776b372bfa188
AngelSantiagoPK <angelsantiago094@gmail.com>
2024-02-29 14:08:05 -0500

    I hope this works. Bonus and water damage

---
+++ lib/game/components/entities/gomi_entity.dart
@@ -0,0 +1,17 @@
+import 'package:flame/components.dart';
+import 'package:gomi/game/components/entities/player.dart';
+
+abstract class GomiEntity extends SpriteAnimationGroupComponent<GomiEntity> {
+  GomiEntity({super.size, super.position, super.anchor});
+  Vector2 velocity = Vector2.zero();
+  final double speed = 0;
+  int direction = 0;
+  final double gravity = 9.8;
+  double jumpCooldown = 1.5;
+  bool gotHit = false;
+  final double jumpForce = 200;
+  bool isGrounded = false;
+  double lastJumpTimestamp = 0.0;
+  final double maxVelocity = 300;
+  final double bounceForce = 200;
+}
