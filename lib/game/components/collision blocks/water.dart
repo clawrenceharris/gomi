9944364fb173fd2c2d76cebd3823a7d3c5f579f2
AngelSantiagoPK <angelsantiago094@gmail.com>
2024-02-29 13:52:01 -0500

    added a bonus level, and water does damage

--- lib/game/components/collision blocks/water.dart
+++ lib/game/components/collision blocks/water.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
 import 'package:flame/collisions.dart';
+import 'package:flame/src/components/position_component.dart';
 import 'package:gomi/game/components/collision%20blocks/collision_block.dart';
+import 'package:gomi/game/components/entities/player.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 class Water extends CollisionBlock {
   Water({position, size}) : super(position: position, size: size);
@@ -10,5 +13,13 @@ class Water extends CollisionBlock {
   FutureOr<void> onLoad() {
     add(RectangleHitbox(collisionType: CollisionType.passive));
     return super.onLoad();
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollision(intersectionPoints, other);
+    if (other is Player) {
+      other.hit();
+    }
   }
 }
