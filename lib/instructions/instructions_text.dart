d46f0594dfad01e8e4cf21543894d15150d1dd7f
Caleb Harris <caleb@Calebs-Air.lan>
2024-03-04 14:03:33 -0500

    modified instructions

--- lib/instructions/instructions_text.dart
+++ lib/instructions/instructions_text.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 
 class InstructionsText {
-  static getStyle({Color color = Colors.white}) {
+  static TextStyle getStyle({Color color = Colors.white, Paint? foreground}) {
     return TextStyle(
         fontFamily: 'Pixel',
-        color: color,
+        color: foreground == null ? color : null,
         fontSize: 16,
+        fontWeight: FontWeight.normal,
+        foreground: foreground,
         decoration: TextDecoration.none);
   }
 }
 
 final List<Widget> instructionTexts = [
   Text(
+      textAlign: TextAlign.center,
       "Help Gomi collect Litter Critters that have polluted the land!\n\nDefeat Litter Critters by jumping on top of them...But be sure you've procured the right color!",
       style: InstructionsText.getStyle()),
   Text(
       "Use the left and right arrow keys to walk.\n\nUse the up arrow or sapce to jump.",
       style: InstructionsText.getStyle()),
   RichText(
+    textAlign: TextAlign.center,
     text: TextSpan(
-      style:
-          InstructionsText.getStyle(color: Colors.white), // Default text style
+      style: InstructionsText.getStyle(), // Default text style
       children: <TextSpan>[
         const TextSpan(
           text: 'Discard water bottles with ',
@@ -32,9 +35,9 @@ final List<Widget> instructionTexts = [
     ),
   ),
   RichText(
+    textAlign: TextAlign.center,
     text: TextSpan(
-      style:
-          InstructionsText.getStyle(color: Colors.white), // Default text style
+      style: InstructionsText.getStyle(), // Default text style
       children: <TextSpan>[
         const TextSpan(
           text: 'Compost tomatoes with  ',
@@ -45,18 +48,33 @@ final List<Widget> instructionTexts = [
       ],
     ),
   ),
-  RichText(
-    text: TextSpan(
-      style:
-          InstructionsText.getStyle(color: Colors.white), // Default text style
-      children: <TextSpan>[
-        const TextSpan(text: 'trash broken light bulbs with '),
-        TextSpan(
-            text: 'Black Gomi.',
-            style: InstructionsText.getStyle(color: Colors.black54)),
-      ],
-    ),
+  Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(
+        textAlign: TextAlign.center,
+        'trash broken light bulbs with ',
+        style: InstructionsText.getStyle(),
+      ),
+      Stack(
+        children: [
+          Text('Black Gomi.',
+              textAlign: TextAlign.center,
+              style: InstructionsText.getStyle(
+                foreground: Paint()
+                  ..style = PaintingStyle.stroke
+                  ..strokeWidth = 5
+                  ..color = Colors.white,
+              )),
+          Text('Black Gomi.',
+              textAlign: TextAlign.center,
+              style: InstructionsText.getStyle(color: Colors.black38)),
+        ],
+      )
+    ],
   ),
+  // Default text style
+
   RichText(
     text: TextSpan(
       style:
